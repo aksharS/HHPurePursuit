edb819a7089bb005c216c0a7a4495f46a4d29c41
Tushaar <Tushaar@10.40.22.24>
2019-10-22 10:17:12 -0400

    added space

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -12,6 +12,7 @@ package frc.robot;
  * to a variable name. This provides flexibility changing wiring, makes checking
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
+ * 
  */
 public class RobotMap {
 
